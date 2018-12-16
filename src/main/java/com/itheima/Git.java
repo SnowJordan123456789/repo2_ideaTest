19edc2f8678d1fcf7235ac68bd9ea46124eec9d8
xsk <369656664@qq.com>
2018-12-16 20:18:45 +0800

    一次测试

---
+++ src/main/java/com/itheima/Git.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("123456789");
+        System.out.println("123456789");
+        System.out.println("123456789");
+        System.out.println("123456789");
+    }
+}
